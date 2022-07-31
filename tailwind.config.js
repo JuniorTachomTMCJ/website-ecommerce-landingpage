c8b13f2e7a9ba3359e3f6cd447915f7b22a01871
juniortachom <constantjunior30@gmail.com>
2022-07-31 20:50:09 +0100

    rename dist to docs for github pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./dist/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./docs/**/*.{html,js}"],
   theme: {
     container: {
       padding: "16px",
