ac9dc430ebecbf6fdd5cc15f6b0a0fe5b4f1c4c4
juniortachom <constantjunior30@gmail.com>
2022-08-01 01:49:43 +0100

    fix

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,9 +1,13 @@
 window.addEventListener("load", () => {
-  document.body.classList.add("loaded");
+  setTimeout(() => {
+    document.body.classList.add("loaded");
+  }, 500);
 });
 
 let menuCheckBox = document.getElementById("menu");
 let searchCheckBox = document.querySelector("#search");
+let subMenu = document.getElementById("sub-menu");
+let header = document.querySelector("header");
 
 let newsItems = document.querySelectorAll(".new-item");
 let ciblesItems = document.querySelectorAll(".cible-item");
@@ -15,6 +19,12 @@ document.addEventListener("scroll", () => {
 
   let doc = document.documentElement;
   let scrollTop = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+
+  // if (scrollTop > 70) {
+  //   header.classList.replace("relative", "fixed");
+  // } else {
+  //   header.classList.remove("fixed", "relative");
+  // }
 
   newsItems.forEach((item) => {
     let topDistance = item.offsetTop;
@@ -62,3 +72,94 @@ document.querySelectorAll("nav a").forEach((link) => {
     menuCheckBox.checked = false;
   });
 });
+
+// Animation mouse drag slide
+let isDown = false;
+let startX;
+let scrollLeft;
+const slider = document.querySelector(".categories-items");
+
+const end = () => {
+  isDown = false;
+  slider.classList.remove("active");
+};
+
+const start = (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX || e.touches[0].pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+};
+
+const move = (e) => {
+  if (!isDown) return;
+
+  e.preventDefault();
+  const x = e.pageX || e.touches[0].pageX - slider.offsetLeft;
+  const dist = x - startX;
+  slider.scrollLeft = scrollLeft - dist;
+};
+
+(() => {
+  slider.addEventListener("mousedown", start);
+  slider.addEventListener("touchstart", start);
+
+  slider.addEventListener("mousemove", move);
+  slider.addEventListener("touchmove", move);
+
+  slider.addEventListener("mouseleave", end);
+  slider.addEventListener("mouseup", end);
+  slider.addEventListener("touchend", end);
+})();
+
+// Hero slider
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.querySelectorAll(".controls input");
+  let next = document.querySelector(".next-btn");
+  let prev = document.querySelector(".prev-btn");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].checked = false;
+  }
+
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].checked += true;
+  console.log(slideIndex - 1 == 0);
+
+  if (slideIndex - 1 == 0) {
+    prev.classList.add("desactived");
+  } else {
+    prev.classList.remove("desactived");
+  }
+  if (slideIndex - 1 == slides.length - 1) {
+    next.classList.add("desactived");
+  } else {
+    next.classList.remove("desactived");
+  }
+}
